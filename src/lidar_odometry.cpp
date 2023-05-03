336eed6463efb0151acab4c2a963dfe5afc0233f
janusz <januszbedkowski@gmail.com>
2023-05-03 14:56:13 +0200

    gui

--- src/lidar_odometry.cpp
+++ src/lidar_odometry.cpp
@@ -5,7 +5,189 @@
 #include <Fusion.h>
 #include <map>
 
-int main(int argc, char *argv[]){
+#include <imgui.h>
+#include <imgui_impl_glut.h>
+#include <imgui_impl_opengl2.h>
+#include <ImGuizmo.h>
+#include <imgui_internal.h>
 
+#include <glew.h>
+#include <GL/freeglut.h>
+
+float rotate_x = 0.0, rotate_y = 0.0;
+float translate_x, translate_y = 0.0;
+float translate_z = -50.0;
+const unsigned int window_width = 800;
+const unsigned int window_height = 600;
+static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+int mouse_old_x, mouse_old_y;
+bool gui_mouse_down{ false };
+int mouse_buttons = 0; 
+float mouse_sensitivity = 1.0;
+
+void mouse(int glut_button, int state, int x, int y) {
+    ImGuiIO& io = ImGui::GetIO();
+    io.MousePos = ImVec2((float)x, (float)y);
+    int button = -1;
+    if (glut_button == GLUT_LEFT_BUTTON) button = 0;
+    if (glut_button == GLUT_RIGHT_BUTTON) button = 1;
+    if (glut_button == GLUT_MIDDLE_BUTTON) button = 2;
+    if (button != -1 && state == GLUT_DOWN)
+        io.MouseDown[button] = true;
+    if (button != -1 && state == GLUT_UP)
+        io.MouseDown[button] = false;
+
+    if (!io.WantCaptureMouse)
+    {
+        if (state == GLUT_DOWN) {
+            mouse_buttons |= 1 << glut_button;
+        }
+        else if (state == GLUT_UP) {
+            mouse_buttons = 0;
+        }
+        mouse_old_x = x;
+        mouse_old_y = y;
+    }
+}
+
+void wheel(int button, int dir, int x, int y)
+{
+    if (dir > 0)
+    {
+        translate_z -= 0.05f * translate_z;
+    }
+    else
+    {
+        translate_z += 0.05f * translate_z;
+    }
+    return;
+}
+
+void reshape(int w, int h) {
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60.0, (GLfloat)w / (GLfloat)h, 0.01, 10000.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void motion(int x, int y) {
+    ImGuiIO& io = ImGui::GetIO();
+    io.MousePos = ImVec2((float)x, (float)y);
+
+    if (!io.WantCaptureMouse)
+    {
+        float dx, dy;
+        dx = (float)(x - mouse_old_x);
+        dy = (float)(y - mouse_old_y);
+
+        gui_mouse_down = mouse_buttons > 0;
+        if (mouse_buttons & 1) {
+            rotate_x += dy * 0.2f * mouse_sensitivity;
+            rotate_y += dx * 0.2f * mouse_sensitivity;
+        }
+        if (mouse_buttons & 4) {
+            translate_x += dx * 0.5f * mouse_sensitivity;
+            translate_y -= dy * 0.5f * mouse_sensitivity;
+        }
+       
+        mouse_old_x = x;
+        mouse_old_y = y;
+    }
+    glutPostRedisplay();
+}
+
+void display() {
+    ImGuiIO& io = ImGui::GetIO();
+    glViewport(0, 0, (GLsizei)io.DisplaySize.x, (GLsizei)io.DisplaySize.y);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    float ratio = float(io.DisplaySize.x) / float(io.DisplaySize.y);
+    
+    glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+    
+    reshape((GLsizei)io.DisplaySize.x, (GLsizei)io.DisplaySize.y);
+    glTranslatef(translate_x, translate_y, translate_z);
+    glRotatef(rotate_x, 1.0, 0.0, 0.0);
+    glRotatef(rotate_y, 0.0, 0.0, 1.0);
+      
+    glBegin(GL_LINES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(100, 0.0f, 0.0f);
+
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 100, 0.0f);
+
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, 100);
+    glEnd();
+  
+    ImGui_ImplOpenGL2_NewFrame();
+    ImGui_ImplGLUT_NewFrame();
+
+    //my_display_code();
+    //if(is_ndt_gui)ndt_gui();
+    //if(is_icp_gui)icp_gui();
+    //if(is_pose_graph_slam)pose_graph_slam_gui();
+    //if(is_registration_plane_feature)registration_plane_feature_gui();
+    //if(is_manual_analisys)observation_picking_gui();
+    //project_gui();
+    
+    ImGui::Render();
+    ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+    
+    glutSwapBuffers();
+    glutPostRedisplay();
+}
+
+bool initGL(int* argc, char** argv) {
+    glutInit(argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+    glutInitWindowSize(window_width, window_height);
+    glutCreateWindow("lidar_odometry");
+    glutDisplayFunc(display);
+    glutMotionFunc(motion);
+
+    // default initialization
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glEnable(GL_DEPTH_TEST);
+
+    // viewport
+    glViewport(0, 0, window_width, window_height);
+
+    // projection
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60.0, (GLfloat)window_width / (GLfloat)window_height, 0.01, 10000.0);
+    glutReshapeFunc(reshape);
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+
+    ImGui::StyleColorsDark();
+    ImGui_ImplGLUT_Init();
+    ImGui_ImplGLUT_InstallFuncs();
+    ImGui_ImplOpenGL2_Init();
+    return true;
+}
+
+int main(int argc, char *argv[]){
+    initGL(&argc, argv);
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+    glutMouseWheelFunc(wheel);
+    glutMainLoop();
+
+    ImGui_ImplOpenGL2_Shutdown();
+    ImGui_ImplGLUT_Shutdown();
+
+    ImGui::DestroyContext();
     return 0;
 }

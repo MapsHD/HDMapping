99bbbfe6f413e8f30355505751e8fa07ecc9d1fe
janusz <januszbedkowski@gmail.com>
2023-10-15 11:25:28 +0200

    v0.24 candidate

--- src/multi_view_tls_registration.cpp
+++ src/multi_view_tls_registration.cpp
@@ -3194,7 +3194,7 @@ bool initGL(int *argc, char **argv)
     glutInit(argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
     glutInitWindowSize(window_width, window_height);
-    glutCreateWindow("multi_view_tls_registration_step_2 v0.23");
+    glutCreateWindow("multi_view_tls_registration_step_2 v0.24");
     glutDisplayFunc(display);
     glutMotionFunc(motion);
 

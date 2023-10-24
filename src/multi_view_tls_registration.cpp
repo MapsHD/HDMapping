2ecf7ca3d679db5ade7527a65c573b2327df2ecc
janusz <januszbedkowski@gmail.com>
2023-10-24 14:00:33 +0200

    export trajecotyr in obslam3 fromat

--- src/multi_view_tls_registration.cpp
+++ src/multi_view_tls_registration.cpp
@@ -1564,7 +1564,7 @@ void project_gui()
                     }
                 }
             }
-            if (ImGui::Button("save all marked trajectories to csv (x,y,z,r00,r01,r02,r10,r11,r12,r20,r21,r22)"))
+            if (ImGui::Button("save all marked trajectories to csv (timestamp,x,y,z,r00,r01,r02,r10,r11,r12,r20,r21,r22)"))
             {
                 std::shared_ptr<pfd::save_file> save_file;
                 std::string output_file_name = "";
@@ -1593,8 +1593,8 @@ void project_gui()
                                     const auto &m = p.local_trajectory[i].m_pose;
                                     Eigen::Affine3d pose = p.m_pose * m;
                                     pose.translation() += session.point_clouds_container.offset;
-
-                                    outfile << pose(0, 3) << "," << pose(1, 3) << "," << pose(2, 3) << "," << pose(0, 0) << "," << pose(0, 1) << "," << pose(0, 2) << "," << pose(1, 0) << "," << pose(1, 1) << "," << pose(1, 2) << "," << pose(2, 0) << "," << pose(2, 1) << "," << pose(2, 2) << std::endl;
+                                    outfile << std::setprecision(20);
+                                    outfile << p.local_trajectory[i].timestamp << "," << pose(0, 3) << "," << pose(1, 3) << "," << pose(2, 3) << "," << pose(0, 0) << "," << pose(0, 1) << "," << pose(0, 2) << "," << pose(1, 0) << "," << pose(1, 1) << "," << pose(1, 2) << "," << pose(2, 0) << "," << pose(2, 1) << "," << pose(2, 2) << std::endl;
                                 }
                             }
                         }
@@ -1602,7 +1602,49 @@ void project_gui()
                     }
                 }
             }
-        }
+            if (ImGui::Button("save all marked trajectories to csv (timestamp,x,y,z,qx,qy,qz,qw)"))
+            {
+                std::shared_ptr<pfd::save_file> save_file;
+                std::string output_file_name = "";
+                ImGui::PushItemFlag(ImGuiItemFlags_Disabled, (bool)save_file);
+                const auto t = [&]()
+                {
+                    auto sel = pfd::save_file("Save las or csv file", "C:\\").result();
+                    output_file_name = sel;
+                    std::cout << "las or csv file to save: '" << output_file_name << "'" << std::endl;
+                };
+                std::thread t1(t);
+                t1.join();
+
+                if (output_file_name.size() > 0)
+                {
+                    std::ofstream outfile(output_file_name);
+                    if (outfile.good())
+                    {
+                        for (auto &p : session.point_clouds_container.point_clouds)
+                        {
+                            if (p.visible)
+                            {
+
+                                for (int i = 0; i < p.local_trajectory.size(); i++)
+                                {
+                                    const auto &m = p.local_trajectory[i].m_pose;
+                                    Eigen::Affine3d pose = p.m_pose * m;
+                                    pose.translation() += session.point_clouds_container.offset;
+                                    Eigen::Quaterniond q(pose.rotation());
+                                    outfile << std::setprecision(20);
+                                    outfile << p.local_trajectory[i].timestamp << "," << 
+                                        pose(0, 3) << "," << pose(1, 3) << "," << pose(2, 3) << "," << 
+                                        q.x() << "," << q.y() << "," << q.z() << "," << q.w() << std::endl;
+                                }
+                            }
+                        }
+                        outfile.close();
+                    }
+                }
+            }
+        }//simple gui
+        
         ImGui::Separator();
         ImGui::Separator();
 

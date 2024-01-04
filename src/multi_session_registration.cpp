92b6ed05d6e36203cb1df32bafa13df55d0a69b4
janusz <januszbedkowski@gmail.com>
2024-01-04 23:39:41 +0100

    release v0.30 candidate

--- src/multi_session_registration.cpp
+++ src/multi_session_registration.cpp
@@ -1715,7 +1715,7 @@ bool initGL(int *argc, char **argv)
     glutInit(argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
     glutInitWindowSize(window_width, window_height);
-    glutCreateWindow("multi_session_registration_step_3 v0.29");
+    glutCreateWindow("multi_session_registration_step_3 v0.30");
     glutDisplayFunc(display);
     glutMotionFunc(motion);
 

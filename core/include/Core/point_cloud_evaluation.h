1a14fdeedbf31317c002a447922d7b60cb8663dc
janusz <januszbedkowski@gmail.com>
2026-08-11 22:10:01 +0200

    point cloud evaluation place holder

---
+++ core/include/Core/point_cloud_evaluation.h
@@ -0,0 +1,23 @@
+#ifndef _POINT_CLOUD_EVALUATION_H_
+#define _POINT_CLOUD_EVALUATION_H_
+
+#include <Eigen/Eigen>
+#include <vector>
+
+// other tools https://deepwiki.com/fwilliams/point-cloud-utils/5.3-point-cloud-metrics
+// https://arxiv.org/pdf/2604.18205
+class PointCloudEvaluation
+{
+public:
+    PointCloudEvaluation() = default;
+    ~PointCloudEvaluation() = default;
+
+    double ChamferDistance(const std::vector<Eigen::Vector3d>& cloud1, const std::vector<Eigen::Vector3d>& cloud2);
+    double HausdorffDistance(const std::vector<Eigen::Vector3d>& cloud1, const std::vector<Eigen::Vector3d>& cloud2);
+    double EarthMoverDistance(const std::vector<Eigen::Vector3d>& cloud1, const std::vector<Eigen::Vector3d>& cloud2);
+    double Precision(const std::vector<Eigen::Vector3d>& cloud1, const std::vector<Eigen::Vector3d>& cloud2);
+    double Recall(const std::vector<Eigen::Vector3d>& cloud1, const std::vector<Eigen::Vector3d>& cloud2);
+    double F1Score(const std::vector<Eigen::Vector3d>& cloud1, const std::vector<Eigen::Vector3d>& cloud2);
+};
+
+#endif
---
+++ core/src/point_cloud_evaluation.cpp
@@ -0,0 +1,49 @@
+#include "Core/point_cloud_evaluation.h"
+
+double PointCloudEvaluation::ChamferDistance(const std::vector<Eigen::Vector3d>& cloud1, const std::vector<Eigen::Vector3d>& cloud2)
+{
+    // Placeholder implementation for Chamfer Distance calculation
+    // This function should compute the Chamfer distance between two point clouds
+    // For now, it returns a dummy value
+    return 0.0;
+}
+
+double PointCloudEvaluation::HausdorffDistance(const std::vector<Eigen::Vector3d>& cloud1, const std::vector<Eigen::Vector3d>& cloud2)
+{
+    // Placeholder implementation for Hausdorff Distance calculation
+    // This function should compute the Hausdorff distance between two point clouds
+    // For now, it returns a dummy value
+    return 0.0;
+}
+
+double PointCloudEvaluation::EarthMoverDistance(const std::vector<Eigen::Vector3d>& cloud1, const std::vector<Eigen::Vector3d>& cloud2)
+{
+    // Placeholder implementation for Earth Mover's Distance calculation
+    // This function should compute the Earth Mover's distance between two point clouds
+    // For now, it returns a dummy value
+    return 0.0;
+}
+
+double PointCloudEvaluation::Precision(const std::vector<Eigen::Vector3d>& cloud1, const std::vector<Eigen::Vector3d>& cloud2)
+{
+    // Placeholder implementation for Precision calculation
+    // This function should compute the precision between two point clouds
+    // For now, it returns a dummy value
+    return 0.0;
+}
+
+double PointCloudEvaluation::Recall(const std::vector<Eigen::Vector3d>& cloud1, const std::vector<Eigen::Vector3d>& cloud2)
+{
+    // Placeholder implementation for Recall calculation
+    // This function should compute the recall between two point clouds
+    // For now, it returns a dummy value
+    return 0.0;
+}
+
+double PointCloudEvaluation::F1Score(const std::vector<Eigen::Vector3d>& cloud1, const std::vector<Eigen::Vector3d>& cloud2)
+{
+    // Placeholder implementation for F1 Score calculation
+    // This function should compute the F1 score between two point clouds
+    // For now, it returns a dummy value
+    return 0.0;
+}
